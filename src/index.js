0ce3a5190f84a6826f4aa115885ce6739b880fae
unknown <brianna.hy.ta@gmail.com>
2021-02-06 16:48:59 -0500

    setting up the home page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+export default class TitlePage extends React.Component {
+
+  render() {
+    return (
+      <div className="box">
+        <h1>Date Idea Generator</h1>
+        <h3>Click start to generate a list of fun dates!</h3>
+        <div style={{textAlign: "center"}}>
+          <button class="btn">Start</button>
+        </div>
+      </div>
+    )
+  }
+} 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TitlePage />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
